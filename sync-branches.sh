a364e4952ffde1362133731710cc6424302c4f5e
Josef Cacek <josef.cacek@gmail.com>
2021-01-06 14:00:30 +0100

    Sync-brach update

--- sync-branches.sh
+++ sync-branches.sh
@@ -7,13 +7,11 @@ git commit -a -m "Comment not provided! Don't forget to commit next time."
 git remote add upstream git@github.com:hazelcast-dockerfiles/hazelcast-enterprise-snapshot.git
 
 git fetch upstream
-for branchref in `git branch -r`; do
-  if [[ "$branchref" =~ ^upstream/ ]]; then
-    branch="${branchref##upstream/}"
-    git checkout "$branch"
-    git reset --hard tmp-master
-    git push upstream $branch
-  fi
+for branchref in $(git branch -r --list 'upstream/*' | grep -v upstream/HEAD); do
+  branch="${branchref##upstream/}"
+  git checkout "$branch"
+  git reset --hard tmp-master
+  git push upstream $branch
 done
 
 git checkout master
