712dd8c1d54f5fb36a5a24b598473b02f3996a7b
Josef Cacek <josef.cacek@gmail.com>
2021-01-06 12:59:12 +0100

    fix sync-branches script

--- sync-branches.sh
+++ sync-branches.sh
@@ -4,7 +4,7 @@ git branch -D tmp-master
 git checkout -b tmp-master
 git commit -a -m "Comment not provided! Don't forget to commit next time."
 
-git remote add upstream git@github.com:hazelcast-dockerfiles/hazelcast-snapshot.git
+git remote add upstream git@github.com:hazelcast-dockerfiles/hazelcast-enterprise-snapshot.git
 
 git fetch upstream
 for branchref in `git branch -r`; do
